331edbd18d4cf3a0c11f8a6f14410608fed843bb
jiang <18862241347@163.com>
2017-02-19 21:31:59 +0800

    添加一个people

---
+++ BKNetwork/BKNetwork/Persion.h
@@ -0,0 +1,14 @@
+//
+//  Persion.h
+//  BKNetwork
+//
+//  Created by jiang on 2017/2/19.
+//  Copyright © 2017年 jiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Persion : NSObject
+@property (nonnull,copy) NSString *name;
+
+@end
---
+++ BKNetwork/BKNetwork/Persion.m
@@ -0,0 +1,13 @@
+//
+//  Persion.m
+//  BKNetwork
+//
+//  Created by jiang on 2017/2/19.
+//  Copyright © 2017年 jiang. All rights reserved.
+//
+
+#import "Persion.h"
+
+@implementation Persion
+
+@end
